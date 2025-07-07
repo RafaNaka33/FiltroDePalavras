75fb4d1d26fbd481e210a21bc418210fb1698cb0
RafaNaka33 <rafael.kazuo.tn@gmail.com>
2025-07-07 09:09:32 -0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+const botaoMostraPalavras = document.querySelector('#botao-extrair');
+botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
+function mostraPalavraChave(){
+    alert('Estou Funcionando!')
+}
